51a48882ab888ce38505779383a33a8d064d9c08
hvd_007 <harshdixit1981@gmail.com>
2022-06-13 20:14:44 +0530

    first

---
+++ Inc/main.h
@@ -0,0 +1,30 @@
+/*
+ * main.h
+ *
+ *  Created on: 09-Jun-2022
+ *      Author: harsh
+ */
+
+#ifndef MAIN_H_
+//Some macros are here for implementing in function
+
+#define MAX_TASKS 4
+#define SIZE_TASK_STACK 1024U
+#define SIZE_SICHED_STACK 1024U
+
+#define SRAM_START 0x20000000U
+#define SRAM_SIZE ((96)*1024)
+#define SRAM_END ((SRAM_START)+(SRAM_SIZE))
+
+#define T1_STACK_START  SRAM_END
+#define T2_STACK_START  ((SRAM_END) - ( 1* SIZE_TASK_STACK) )
+#define T3_STACK_START  ((SRAM_END) - (2* SIZE_TASK_STACK) )
+#define T4_STACK_START  ((SRAM_END) - (3 * SIZE_TASK_STACK))
+#define SCHED_STACK_START ((SRAM_END)- (4 * SIZE_TASK_STACK))
+#define TICK_HZ 1000U
+#define HSI_CLOCK 16000000U
+#define SYSTICK_TIM_CLK HSI_CLOCK
+#define DUMMY_XPSR 0x01000000U
+
+
+#endif /* MAIN_H_ */
